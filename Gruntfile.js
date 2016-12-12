66f4b91e79829ce415269ddbae7c89abb0209bae
Eric Meyer <emeyer@8thlight.com>
2016-12-12 09:05:49 -0600

    Update Gruntfile
     * Keep SASS/CSS tasks around
         * CSS is built separately from the JavaSscript

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,42 +3,13 @@
 module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
   grunt.initConfig({
-    //********************************
-    //Tasks in alphabetical order
-    //********************************
 
     clean: {
-      tmp: ['.tmp/*'],
-      dist: ['dist/*']
-    },
-
-    coffee: {
-      src: {
-        files: [{
-          expand: true,
-          cwd: 'scripts',
-          src: '**/**/*.coffee',
-          dest: '.tmp/scripts',
-          ext: '.js'
-        }]
-      },
-
-      spec: {
-        files: [{
-          expand: true,
-          cwd: 'spec',
-          src: '**/**/*.coffee',
-          dest: '.tmp/spec',
-          ext: '.js'
-        }]
-      }
+      sass: ['dist/*.scss'],
+      tmp: ['.tmp/*']
     },
 
     copy: {
-      namespace: {
-        src: 'scripts/namespace.js',
-        dest: '.tmp/scripts/namespace.js'
-      },
       css: {
         src: '.tmp/styles/dash-banner.css',
         dest: 'dist/dash-banner.css'
@@ -46,17 +17,6 @@ module.exports = function (grunt) {
       scss: {
         src: 'styles/dash-banner.scss',
         dest: 'dist/dash-banner.scss'
-      }
-    },
-
-    jst: {
-      compile: {
-        options: {
-          namespace: 'DashBannerJST'
-        },
-        files: {
-          ".tmp/scripts/dash_banner/templates.js": ["scripts/dash_banner/**/*.ejs"]
-        }
       }
     },
 
@@ -68,46 +28,14 @@ module.exports = function (grunt) {
       }
     },
 
-    uglify: {
-      options: {
-        mangle: true
-      },
-
-      dist: {
-        files: {
-          'dist/dash-banner.min.js': [
-            '.tmp/scripts/namespace.js',
-            '.tmp/scripts/dash_banner/templates.js',
-            '.tmp/scripts/dash_banner/view.js',
-          ]
-        }
-      }
-    },
   });
 
-  //********************************
-  //Builds
-  //********************************
-
-  grunt.registerTask('build:dist', [
-                     'clean:tmp',
-                     'clean:dist',
-                     'jst',
-                     'coffee:src',
-                     'sass', // Compiles sass for sample app
-                     'copy:namespace',
-                     'copy:scss',
-                     'uglify'
-
+  grunt.registerTask("build:dist", [
+    "clean",
+    "sass",
+    "copy:scss"
   ]);
 
-  grunt.registerTask('build:spec', [
-                     'clean:tmp',
-                     'jst',
-                     'coffee:src',
-                     'sass',
-                     'coffee:spec'
-  ]);
+  grunt.registerTask("default", ["build:dist"]);
 
-  grunt.registerTask('default', ['build:dist']);
 };
