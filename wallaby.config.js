c8e539e265167ee33542f6710ae2072228795731
Eric Meyer <emeyer@8thlight.com>
2017-01-04 14:45:36 -0600

    Get Wallaby working

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,3 +1,4 @@
+var webpack = require("webpack");
 var wallabyWebpack = require('wallaby-webpack');
 var webpackConfig = require('./webpack.config');
 var path = require("path");
@@ -8,16 +9,21 @@ module.exports = function (wallaby) {
   webpackPostprocessor = wallabyWebpack({
     module: {
       loaders: [{
-        include: path.join(PROJECT_ROOT, "scripts/dash_banner"),
+        include: path.join(wallaby.projectCacheDir, "scripts", "dash_banner"),
         loader: "ejs-compiled",
         test: /\.ejs$/
       }]
     },
+    plugins: webpackConfig.plugins,
     resolve: {
       alias: {
         "dash_banner": path.join(wallaby.projectCacheDir, "scripts", "dash_banner"),
         "sample_app": path.join(wallaby.projectCacheDir, "scripts", "sample_app")
-      }
+      },
+      modulesDirectories: [
+        path.join(PROJECT_ROOT, "node_modules"),
+        path.join(PROJECT_ROOT, "bower_components")
+      ]
     }
   })
 
@@ -34,7 +40,7 @@ module.exports = function (wallaby) {
       {pattern: "scripts/**/*.js", load: false}
     ],
 
-    postprocessor: wallabyWebpack(webpackConfig),
+    postprocessor: webpackPostprocessor,
 
     setup: function () {
       window.__moduleBundler.loadTests();
